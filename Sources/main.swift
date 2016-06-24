57ed9a254abaeb81ad689918e2b4a929f5673e9b
Igor <igor@appirio.com>
2016-06-24 14:00:40 +0900

    Swift JSON test

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,13 +7,13 @@ import Curassow
 serve { _ in
   
     let dict: [String:Any] = ["a":1, "b":0.1, "c": true, "d":[2, 4, 5, 7, 8]]
-    let response = Response(.Ok, contentType: "application/json", body: "RESTfull Heroku Swift server")
-    do {
-        let jsonString = try dict.jsonEncodedString()
-        response.appendBody(string: jsonString)
-    } catch {
+    let response = Response(.Ok, contentType: "application/json", body: "{\"msg\": \"Heroku Swift server\"}")
+    //do {
+    //    let jsonString = try dict.jsonEncodedString()
+    //    response.appendBody(string: jsonString)
+    //} catch {
         
-    }
+    //}
     //return Response(.Ok, contentType: "text/plain", body: "RESTfull Heroku Swift server")
     return response
 }
